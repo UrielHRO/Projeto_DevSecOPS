4103145d32b197a4c0b5d303a21df80a3ae263e0
Uriel Ribeiro <uriel.h.ribeiro@gmail.com>
2025-10-15 12:54:55 +0000

    vulnerabilidade token

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   res.status(200).json({ mensagem: 'API funcionando corretamente!' });
 };
 
-const apiKey = "2131231299425@67934S";
+const token = "apiKey_123456789ABC";
 
 
 
